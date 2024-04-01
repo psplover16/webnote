34e55561de20a8b615ea739ab0e0142dd5c8a4a0
psplover16 <52639804+psplover16@users.noreply.github.com>
2024-04-01 08:35:07 +0800

    try3

---
+++ dist/assets/frontEngineering-1gzxUNGC.js
@@ -0,0 +1,34 @@
+import{r as u,o as i,c as n,a as t,b as s,t as c,n as d,d as p,e as _,g as r}from"./index-Desngf0b.js";const m={key:0,type:"I"},f={__name:"ch1",props:{title:String},setup(o){const e=u(!1);return(a,l)=>(i(),n("div",null,[t("h2",{onClick:l[0]||(l[0]=v=>e.value=!e.value)},[s(c(o.title)+" ",1),t("div",{class:d(["isShow",e.value?"":"rotate180"])},"^",2)]),e.value?(i(),n("ol",m)):p("",!0)]))}},h={key:0,type:"I"},g=_(`<li><pre>  import { defineConfig, loadEnv } from &#39;vite&#39;
+  import vue from &#39;@vitejs/plugin-vue&#39;
+  import path from &quot;path&quot;
+
+  export default defineConfig((command, mode, ssrBuild) =&gt; {
+    const env = loadEnv(mode, process.cwd())
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          &#39;@&#39;: [path.resolve(__dirname, &#39;./src&#39;)] //設置路徑代表的東西
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            // additionalData: \`@import &quot;@/styles/main.scss&quot;;\`, // 将你的全局样式文件引入到每个组件
+          },
+        },
+      },
+      envDir: path.resolve(__dirname, &quot;./env&quot;),
+    }
+  })
+        </pre></li><li><ul><li>defineConfig 是 Vite 中用於建立配置物件的函數。 透過呼叫 defineConfig 函數，你可以建立一個包含 Vite 建構所需各種選項的配置物件。</li><li>在 defineConfig 函數中，可以傳入一個函數，該函數接收三個參數：command、mode 和 ssrBuild。 <ul><li>command: 目前執行的指令，可能的取值為 &quot;build&quot;、&quot;serve&quot; 或 &quot;ssr&quot;。 分別代表建置、開發伺服器和伺服器端渲染。</li><li>mode: 目前的模式，可能的取值為 &quot;development&quot; 或 &quot;production&quot;。 分別代表開發模式和生產模式。</li><li>ssrBuild: 是否是伺服器端渲染（SSR）的建置。 如果是 SSR 構建，則值為 true；否則為 false。</li></ul></li></ul></li><li>__dirname，Node.js 中的一個全域變量，用於取得目前執行檔所在的目錄的路徑。</li><li>resolve，配置模塊解析行為 <ul><li>alias，設置路徑別名</li></ul></li><li>css，配置css相關 <ul><li> preprocessorOptions，指定處理器的配置 <ul><li> scss，指定了預處理器的類型 <ul><li>additionalData，用於指定額外的scss數據</li></ul></li></ul></li></ul></li><li> envDir，配置env的位置 <ul><li>path.resolve()，Node.js 中的一個方法，用於將多個路徑解析為一個絕對路徑。</li></ul></li>`,6),S=[g],b={__name:"ch2",props:{title:String},setup(o){const e=u(!1);return(a,l)=>(i(),n("div",null,[t("h2",{onClick:l[0]||(l[0]=v=>e.value=!e.value)},[s(c(o.title)+" ",1),t("div",{class:d(["isShow",e.value?"":"rotate180"])},"^",2)]),e.value?(i(),n("ol",h,S)):p("",!0)]))}},q={key:0,type:"I"},C=t("li",null,"Vite 預設使用現代瀏覽器的原生 ES 模組匯入功能，但在某些情況下，仍可能需要對程式碼進行轉譯，以確保程式碼在舊版瀏覽器中的相容性。",-1),y=t("li",null," Polyfill API修復功能，Vite內建 ",-1),V=t("li",null,"Babel 是 JavaScript 的編譯器(compiler)工具，可以把 ES6 標準以上的新語法轉換成最基本的 JS 語法",-1),k=t("li",null,[s(" Vite，Babel引用 "),t("br"),s(" npm i vite-plugin-babel "),t("pre",null,`  // vite.config.js
+  import babel from 'vite-plugin-babel';
+
+  export default defineConfig({
+    plugins: [
+      vue(),
+      // 使用 Babel 插件
+      babel(),
+    ],
+  });
+        `)],-1),B=[C,y,V,k],$={__name:"ch3",props:{title:String},setup(o){const e=u(!1);return(a,l)=>(i(),n("div",null,[t("h2",{onClick:l[0]||(l[0]=v=>e.value=!e.value)},[s(c(o.title)+" ",1),t("div",{class:d(["isShow",e.value?"":"rotate180"])},"^",2)]),e.value?(i(),n("ol",q,B)):p("",!0)]))}},w={__name:"frontEngineering",setup(o){return(e,a)=>(i(),n("div",null,[r(f,{title:"Webpack打包工具的配置和使用"}),r(b,{title:"config解析"}),r($,{title:"babel / Polyfill"})]))}};export{w as default};
---
+++ dist/assets/webpage-PDnHXBg9.js
@@ -0,0 +1,14 @@
+import{r,o as l,c as i,a as s,b as c,t as m,n as u,d as p,e as d,g as n}from"./index-Desngf0b.js";const _={key:0,type:"I"},v=d(`<li> cookie <ul><li>只有4K大小</li><li>由伺服器發送到客戶端並保存在客戶端的小型文字文件</li><li>以設定 Cookie 的過期時間，可以在不同頁面之間共用數據</li><li>與server溝通時，會把用到的cookie全部丟給server (幾乎都是後端控制)</li><li><pre>fetch(&#39;https://example.com/api/data&#39;, {
+  method: &#39;GET&#39;,
+  credentials: &#39;include&#39;
+})
+            </pre><ul>credentials <li>omit，預設值，不攜帶</li><li>same-origin 表示請求只會在當前域下發送，會攜帶當前域下符合條件的cookie</li><li>include&#39;表示請求會攜帶所有符合條件的Cookie，不僅限於當前域</li></ul></li><li> form的方式，會自動將cookie添加至請求 </li></ul></li><li> SessionStorage，資料儲存至關閉頁面會瀏覽器清除 <ul><li>5MB</li><li>sessionStorage.setItem(&#39;key&#39;, &#39;value&#39;);</li><li>const data = sessionStorage.getItem(&#39;key&#39;);</li><li>sessionStorage.removeItem(&#39;key&#39;);</li></ul></li><li> LocalStorage，儲存至清除 <ul><li>5MB</li><li>localStorage.setItem(&#39;key&#39;, &#39;value&#39;);</li><li> const data = localStorage.getItem(&#39;key&#39;);</li><li> localStorage.removeItem(&#39;key&#39;);</li></ul></li>`,3),S=[v],f={__name:"ch1",props:{title:String},setup(o){const e=r(!1);return(a,t)=>(l(),i("div",null,[s("h2",{onClick:t[0]||(t[0]=g=>e.value=!e.value)},[c(m(o.title)+" ",1),s("div",{class:u(["isShow",e.value?"":"rotate180"])},"^",2)]),e.value?(l(),i("ol",_,S)):p("",!0)]))}},k={key:0,type:"I"},h=d(`<li>CORS，伺服器設置CORS，標準安全方式，伺服器端設定 CORS 頭部來允許特定來源存取資源。</li><li>JSONP，在script的src建立請求，限定服務器要支援且限定GET</li><li>代理伺服器，在同源伺服器上建立一個代理接口，將請求轉送到目標伺服器。</li><li> Iframe， 透過 &lt;iframe&gt; 載入跨網域的內容，雖然頁面本身仍然受到同源策略的限制，但在 &lt;iframe&gt; 中載入的內容是由目標伺服器傳回的，不受同源策略的限制。 因此，可以利用 &lt;iframe&gt; 載入跨域的內容來繞過同源策略限制。 <br> iframe溝通，postmessage <ul><li> 父 <pre>   const iframe = document.getElementById(&#39;myFrame&#39;);
+   iframe.contentWindow.postMessage(&#39;Hello from parent&#39;, &#39;http://example.com&#39;);
+            </pre></li><li> 子 <pre>  window.addEventListener(&#39;message&#39;, event =&gt; {
+  if (event.origin === &#39;http://example.com&#39;) {
+    console.log(&#39;Message from parent:&#39;, event.data);
+    // 发送响应
+    event.source.postMessage(&#39;Hello from child&#39;, event.origin);
+  }
+  });
+            </pre></li></ul></li><li>WebSocket，WebSocket 不受同源策略的限制，可以直接進行跨域通訊。</li><li>Nginx 反向代理，有一個中間代理的概念</li>`,6),y=[h],I={__name:"ch2",props:{title:String},setup(o){const e=r(!1);return(a,t)=>(l(),i("div",null,[s("h2",{onClick:t[0]||(t[0]=g=>e.value=!e.value)},[c(m(o.title)+" ",1),s("div",{class:u(["isShow",e.value?"":"rotate180"])},"^",2)]),e.value?(l(),i("ol",k,y)):p("",!0)]))}},C={__name:"webpage",setup(o){return(e,a)=>(l(),i("div",null,[n(f,{title:"cookie / SessionStorage / LocalStorage"}),n(I,{title:"跨域問題"})]))}};export{C as default};
