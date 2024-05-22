339c4b79c212943c15df12975b16a67fc12264fc
IOT-112-04-08\Gary <psplover16>
2024-05-22 14:39:20 +0800

    修正 _plugin-vue_export-helper 這個檔案命名規則上在某些環境下會有問題 第一版

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import path from "path"
 import babel from 'vite-plugin-babel';
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
 // 在 defineConfig 函數中，可以傳入一個函數，該函數接收三個參數：command、mode 和 ssrBuild。 這些參數的意義如下：
@@ -32,6 +35,26 @@ export default defineConfig((command, mode, ssrBuild) => {
     envDir: path.resolve(__dirname, "./env"),
     server: {
       port: 3000 // 将端口设置为 3000
-    }
+    },
+
+    build: {
+      rollupOptions: {
+        output: {
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : '';
+            // substr 是被淘汰語法，因此要改 slice
+            return (
+              driveLetter +
+              name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+            );
+          },
+        },
+      },
+    },
+
+
+
   }
 })
