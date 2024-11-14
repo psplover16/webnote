2fc971f5f6103bbcdb9e7c3ac60e757530c25972
gary_lin <gary_lin@cmoney.com.tw>
2024-11-14 08:57:51 +0800

    新增，title屬性

--- src/views/html.vue
+++ src/views/html.vue
@@ -5,13 +5,16 @@
     <ch3 title="html模板" />
     <ch4 title="注意事項" />
     <ch5 title="ruby標籤" />
+    <ch6 title="少見屬性" />
   </div>
 </template>
 
 <script setup>
+import Ch6 from "./frontEngineering/ch6.vue";
 import ch1 from "./html/ch1.vue";
 import ch2 from "./html/ch2.vue";
 import ch3 from "./html/ch3.vue";
 import ch4 from "./html/ch4.vue";
 import ch5 from "./html/ch5.vue";
+import ch6 from "./html/ch6.vue";
 </script>
---
+++ src/views/html/ch6.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 @click="isShow = !isShow">
+      {{ title }}
+      <div :class="['isShow', isShow ? '' : 'rotate180']">^</div>
+    </h2>
+    <ol type="I" v-if="isShow">
+      <li>
+        title屬性，hover會出現提示
+        <div title="hover出現提示">{{ titleAttr }}</div>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+defineProps({
+  title: String,
+});
+const isShow = ref(false);
+const titleAttr = `<span title="hover出現提示">hover出現提示</span>`;
+</script>
